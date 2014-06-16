92eedc94e43784edd0a98ce802bfb660b9ef9f47
Spencer Eldred <eldredspencer@gmail.com>
2014-06-16 17:01:10 -0600

    Revised tests to have World Cup theme, used block quote

--- lib/string_exercise.rb
+++ lib/string_exercise.rb
@@ -1,11 +1,16 @@
 class StringExecise
 
   def initialize
-    @mystring = "hello world"
-    @numbers = "one, two, three, four, five, six, seven, eight, nine, ten"
-    @long_string = "Once, apon, a, time, there\n was, a, junior, developer\n who, wrote, great, code."
+    @goal = "GOOOOOOOAL!!!!"
+    @teams = "Brazil, Mexico, Cameroon, Croatia, Netherlands, Chile, Australia, Spain, Germany, Ghana, US, Protugal"
+    @long_string = <<-world_cup_news
+      Clint, Dempsey, put, the, U.S.,
+      ahead, in, the, first, minute,
+      of, its, World, Cup, opener.
+    world_cup_news
   end
 
+  # add methods here
 
 
 end
--- spec/string_exercise_spec.rb
+++ spec/string_exercise_spec.rb
@@ -5,39 +5,39 @@ require 'string_exercise'
 describe 'string_exercise' do
   let(:str_ex) { StringExecise.new }
 
-  describe "thestring method" do
-    it "should return hello world" do
-      expect(str_ex.thestring).to eq("hello world")
+  describe "goal method" do
+    it "should return an emphatic goal" do
+      expect(str_ex.goal).to eq("GOOOOOOOAL!!!!")
     end
   end
 
-  describe "nums method" do
-    it "should parse @numbers and return ten items" do
-      expect(str_ex.nums.length).to eq(10)
+  describe "teams method" do
+    it "should parse @teams and return ten items" do
+      expect(str_ex.teams.length).to eq(12)
     end
   end
 
-  describe "return_five method" do
-    it "should return the string 'five' from @numbers" do
-      expect(str_ex.return_five).to eq('five')
+  describe "return_australia method" do
+    it "should return the string 'Australia' from @teams" do
+      expect(str_ex.return_australia).to eq('Australia')
     end
   end
 
-  describe "starts_with_t method" do
-    it "should return the array ['two', 'three', 'ten'] from @numbers" do
-      expect(str_ex.starts_with_t).to eq(['two', 'three', 'ten'])
+  describe "starts_with_c method" do
+    it "should return the array ['Cameroon', 'Croatia', 'Chile'] from @numbers" do
+      expect(str_ex.starts_with_c).to eq(['Cameroon', 'Croatia', 'Chile'])
     end
   end
 
   describe "single_line method" do
     it "should return a single line story from @long_string" do
-      expect(str_ex.single_line).to eq("Once apon a time there was a junior developer who wrote great code.")
+      expect(str_ex.single_line).to eq("Clint Dempsey put the U.S. ahead in the first minute of its World Cup opener.")
     end
   end
 
   describe "capitalize_every_third_word method" do
-    it "should return a string with every third word capitalized" do
-      expect(str_ex.capitalize_every_third_word).to eq("Once apon a Time there was A junior developer Who wrote great Code.")
+    it "should return a string with only every third word capitalized" do
+      expect(str_ex.capitalize_every_third_word).to eq("Clint dempsey put The u.s. ahead In the first Minute of its World cup opener.")
     end
 
   end
