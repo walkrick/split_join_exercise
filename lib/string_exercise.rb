8ded91f6d6a5fe4aef7774cd6354757aeea40629
Spencer Eldred <eldredspencer@gmail.com>
2014-06-16 12:56:38 -0600

    first commit - string exercise

---
+++ lib/string_exercise.rb
@@ -0,0 +1,11 @@
+class StringExecise
+
+  def initialize
+    @mystring = "hello world"
+    @numbers = "one, two, three, four, five, six, seven, eight, nine, ten"
+    @long_string = "Once, apon, a, time, there\n was, a, junior, developer\n who, wrote, great, code."
+  end
+
+
+
+end
---
+++ spec/string_exercise_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'rspec/autorun'
+require 'string_exercise'
+
+describe 'string_exercise' do
+  let(:str_ex) { StringExecise.new }
+
+  describe "thestring method" do
+    it "should return hello world" do
+      expect(str_ex.thestring).to eq("hello world")
+    end
+  end
+
+  describe "nums method" do
+    it "should parse @numbers and return ten items" do
+      expect(str_ex.nums.length).to eq(10)
+    end
+  end
+
+  describe "return_five method" do
+    it "should return the string 'five' from @numbers" do
+      expect(str_ex.return_five).to eq('five')
+    end
+  end
+
+  describe "starts_with_t method" do
+    it "should return the array ['two', 'three', 'ten'] from @numbers" do
+      expect(str_ex.starts_with_t).to eq(['two', 'three', 'ten'])
+    end
+  end
+
+  describe "single_line method" do
+    it "should return a single line story from @long_string" do
+      expect(str_ex.single_line).to eq("Once apon a time there was a junior developer who wrote great code.")
+    end
+  end
+
+end
